c9dda870169e143d2a106880b4584fc1fee0078f
Corrigan Newsome <corrigannewsome@gmail.com>
2021-02-25 13:31:16 -0700

    added code from class

--- main.js
+++ main.js
@@ -13,8 +13,31 @@ it adds that animal at the top of the page and pushes the previous info down
 
 document.addEventListener("DOMContentLoaded", () => {
     //in the future this data comes from a website or database
-    const possibleAnimals = ['tiger', 'lion', 'bear', 'roadrunner', 'toto'];
+    
+    function animal_image (url) {
+        this.url = url,
+        this.size = 50,
+        this.shrink = () => {
+            //need to figure out how to get img img.style = “max-width=50px;”;
+        }
+    }
+    
+    function animal(image, name, desc, link) {
+        this.image = image,
+        this.name = name,
+        this.description = desc,
+        this.url = link
+    }
+    
+    const lion_image = new animal_image('lion.jpg');    
+    const lion = new animal(lion_image,
+                            'lion',
+                            'king of the jungle',
+                            'https://en.wikipedia.org/wiki/Lion');
 
+    const possibleAnimals = ['lion', 'tiger', 'bear', 'roadrunner', 'toto'];
+    const animalObjects   = [lion]
+    
     const animalSelector = document.getElementById("animals");
     for(let i=0; i<possibleAnimals.length; i++) {
         const animalOption = document.createElement('option');
@@ -23,22 +46,28 @@ document.addEventListener("DOMContentLoaded", () => {
     }    
     
     const animalImg = document.createElement('img');
-    const messageDiv = document.getElementById("message");
-          messageDiv.appendChild(animalImg);
-    const optionsDiv = document.getElementById("options");
-          optionsDiv.innerHTML+= possibleAnimals.join(', ');
+    document.body.appendChild(animalImg);
     
     const surveyButton = document.getElementById("submit");
         surveyButton.addEventListener("click", () => {
             
-            
             const errorDiv = document.getElementById("error");
             const selectionIndex = animalSelector.selectedIndex;
-            const inputVal = possibleAnimals[selectionIndex];
-            console.log(selectionIndex, inputVal);
+            
+            const selectedAnimalObject = animalObjects[selectionIndex];
+            const inputVal = selectedAnimalObject.image.url;
+            //console.log(selectionIndex, inputVal);
+            //console.log(animalObjects[selectionIndex]);
             
             //show inputVal image in img tag
-            animalImg.src = `imgs\\${inputVal}.jpg`;
+            animalImg.src = `imgs\\${inputVal}`;
             animalImg.alt = inputVal;
+            
+            console.log(selectedAnimalObject.description);
+            document.getElementById("name").innerHTML = selectedAnimalObject.name;
+            document.getElementById("desc").innerHTML = selectedAnimalObject.description;
+            document.getElementById("url").innerHTML  = selectedAnimalObject.url;
+            document.getElementById("url").href = selectedAnimalObject.url;
+            
         });
 });
